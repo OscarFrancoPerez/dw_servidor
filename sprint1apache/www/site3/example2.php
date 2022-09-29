f537293993ce1e58a368b0c45d7bd0fdcbf82b14
Óscar Franco <ofrancop@fpcoruna.afundacion.org>
2022-09-29 11:32:52 +0200

    Ejercicio2 PHP

---
+++ sprint1apache/www/site3/example2.php
@@ -0,0 +1,11 @@
+<html>
+	<body>
+		<h1>Pagina de bienvenida</h1>
+		<?php
+			function dar_bienvenida($nombre){
+				echo "¡Bienvenid@,  " . $nombre . "!";
+			}
+			dar_bienvenida("Nicolas Cage");
+		?>
+	</body>
+</html>
