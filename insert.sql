d5c8762d5ef38a6bff6b1bb13e3c5be5656c39f3
OscarFrancoPerez <113417165+OscarFrancoPerez@users.noreply.github.com>
2022-10-05 14:09:19 +0200

    Add files via upload

--- insert.sql
+++ insert.sql
@@ -6,7 +6,7 @@ VALUES
 	(2, "Carlos", "Turner", "carlosturner1@mail189.uk", "1234"),
 	(3, "Pedro", "Turner", "pedroturner1@mail189.uk", "1234"),
 	(4, "Juan", "Turner", "Juanturner1@mail189.uk", "1234"),
-	(5, "Yago", "Turner", "yagoturner1@mail189.uk", "1234"),
+	(5, "Yago", "Turner", "yagoturner1@mail189.uk", "1234");
 
 INSERT INTO tJuegos(id, nombre, url_imagen, desarrolladora, publicación) 
 VALUES 
@@ -14,7 +14,7 @@ VALUES
 	(2, "Diablo III", "https://static.wikia.nocookie.net/diablo/images/1/12/DiabloIIIcover.jpg/revision/latest/scale-to-width-down/220?cb=20120507084548&path-prefix=es", "Blizzard", STR_TO_DATE('15-Mayo-2012', '%d-%M-%Y')),
 	(3, "The Elder Scrolls V: Skyrim", "https://static.wikia.nocookie.net/elderscrolls/images/c/c5/Skyrim_Cover.png/revision/latest/scale-to-width-down/350?cb=20160812173034", "Bethesda", STR_TO_DATE('11-Noviembre-2011', '%d-%M-%Y')),
 	(4, "Bayonetta", "https://static.wikia.nocookie.net/bayonetta/images/9/9f/Bayonetta1_HD2009Cover.jpg/revision/latest/scale-to-width-down/350?cb=20200311020905", "SEGA", STR_TO_DATE('29-Octubre-2009', '%d-%M-%Y')),
-	(5, "Grand Theft Auto IV", "https://thumbnails.pcgamingwiki.com/b/b7/Grand_Theft_Auto_IV_cover.jpg/450px-Grand_Theft_Auto_IV_cover.jpg", "Rockstar", STR_TO_DATE('29-Abril-2008', '%d-%M-%Y')),
+	(5, "Grand Theft Auto IV", "https://thumbnails.pcgamingwiki.com/b/b7/Grand_Theft_Auto_IV_cover.jpg/450px-Grand_Theft_Auto_IV_cover.jpg", "Rockstar", STR_TO_DATE('29-Abril-2008', '%d-%M-%Y'));
 
 INSERT INTO tComentarios(id, comentario, juego_id, usuario_id) 
 VALUES 
