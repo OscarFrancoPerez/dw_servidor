f695d4699bca77fd8e3957e5efec0d1ac5b77de5
OscarFrancoPerez <113417165+OscarFrancoPerez@users.noreply.github.com>
2022-10-11 13:26:38 +0200

    Update main.php

--- sprint2phpsql/www/mysite/main.php
+++ sprint2phpsql/www/mysite/main.php
@@ -9,16 +9,12 @@ $result = mysqli_query($db, $query) or die('Query error');
 echo '<table>';
 // Recorrer el resultado
 while ($row = mysqli_fetch_array($result)) {
-    echo '<tr>';
-        echo '<td>';
+    echo '<td>';
             echo $row['nombre'];
-        echo '</td>';
-    echo '</tr>';
-    echo '<tr>';
-        echo '<td>';
+    echo '</td>';
+    echo '<td>';
             echo '<img src=" '.$row[2].'"  width="80" height="120">';
-        echo '</td>';
-    echo '</tr>';
+    echo '</td>';
   echo '<br>';
 }
 echo '</table>';
