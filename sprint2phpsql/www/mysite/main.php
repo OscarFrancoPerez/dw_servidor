9163f1caded9a3129066b0a04109489c052bf218
OscarFrancoPerez <113417165+OscarFrancoPerez@users.noreply.github.com>
2022-10-11 12:41:35 +0200

    Update main.php

--- sprint2phpsql/www/mysite/main.php
+++ sprint2phpsql/www/mysite/main.php
@@ -2,3 +2,16 @@
 $db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
 ?>
 <h1>Conexión establecida</h1>
+<?php
+// Lanzar una query
+$query = 'SELECT * FROM tJuegos';
+$result = mysqli_query($db, $query) or die('Query error');
+// Recorrer el resultado
+while ($row = mysqli_fetch_array($result)) {
+  echo $row['titulo'];
+  echo '<br>';
+  echo $row[2];
+  echo '<br>';
+}
+mysqli_close($db);
+?>
