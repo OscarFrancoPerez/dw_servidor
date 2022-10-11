d21e64f8c4f0601cf0fe6844519e861179fe466a
OscarFrancoPerez <113417165+OscarFrancoPerez@users.noreply.github.com>
2022-10-11 13:08:22 +0200

    Delete update.sql

--- update.sql
+++
@@ -1,9 +0,0 @@
-update tJuegos
-set url_imagen = 'https://pobierzgry24.pl/wp-content/uploads/2018/11/ss1-min.jpg'
-where id = 2;
-update tJuegos
-set url_imagen = 'https://www.mobygames.com/images/covers/l/376858-the-elder-scrolls-v-skyrim-special-edition-xbox-one-front-cover.png'
-where id = 3;
-update tJuegos
-set url_imagen = 'https://www.mobygames.com/images/covers/l/175131-bayonetta-playstation-3-front-cover.jpg'
-where id = 4;
