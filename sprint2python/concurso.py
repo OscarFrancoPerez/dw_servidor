0fc4ce618e5ba0af365a271fed4af870d561d4e2
OscarFrancoPerez <ofrancop@fpcoruna.afundacion.org>
2022-10-17 12:52:39 +0200

    concurso.py

---
+++ sprint2python/concurso.py
@@ -0,0 +1,12 @@
+﻿print("¿Quién saboteo el Nord Stream?")
+print("")
+print("a) Rusia")
+print("b) Bolivia")
+print("c) EEUU")
+print("")
+solucion = input("Introduzca su respuesta: ")
+
+if (solucion == "c"):
+    print("Correcto")
+else:
+	print("Fallaste")
