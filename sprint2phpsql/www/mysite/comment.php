c2b692ec7bcf70d4ed60560ac439f92f53c6a5e7
OscarFrancoPerez <113417165+OscarFrancoPerez@users.noreply.github.com>
2022-10-17 12:32:46 +0200

    Update comment.php

--- sprint2phpsql/www/mysite/comment.php
+++ sprint2phpsql/www/mysite/comment.php
@@ -2,8 +2,8 @@
 $db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
 $juego_id = $_POST['juego_id'];
 $comentario = $_POST['comentario'];
-$query = "INSERT INTO tComentarios(comentario, juego_id, usuario_id)
-VALUES ('".$comentario."',".$juego_id.",NULL)";
+$query = "INSERT INTO tComentarios(comentario, juego_id, usuario_id, fecha)
+VALUES ('".$comentario."',".$juego_id.",NULL, NOW())";
 mysqli_query($db, $query) or die('Error');
 mysqli_close($db);
 ?>
