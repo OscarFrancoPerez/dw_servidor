77343c0e59b87dc8f40b48ff886bedcc1b1378de
OscarFrancoPerez <ofrancop@fpcoruna.afundacion.org>
2022-10-17 13:02:52 +0200

    concurso.py actualizado

--- sprint2python/concurso.py
+++ sprint2python/concurso.py
@@ -1,4 +1,6 @@
-﻿print("¿Quién saboteo el Nord Stream?")
+﻿print("Welcome to Geopoltik Today: El concurso donde cualquier respuesta podría ser cierta hasta que se sepa que pasó.")
+puntuacion=0
+print("¿Quién saboteo el Nord Stream?")
 print("")
 print("a) Rusia")
 print("b) Bolivia")
@@ -8,5 +10,39 @@ solucion = input("Introduzca su respuesta: ")
 
 if (solucion == "c"):
     print("Correcto")
+    puntuacion += 10
 else:
 	print("Fallaste")
+	puntuacion = puntuacion - 5
+
+print("¿Que pasó en Las Mavinas?")
+print("")
+print("a) Partido de futbol Argentina-Inglaterra")
+print("b) Imperialismo Británico en su máximo esplandor")
+print("c) Argentina regala Las Malvinas para caerle mejor a los Ingleses")
+print("")
+solucion = input("Introduzca su respuesta: ")
+
+if (solucion == "b"):
+    print("Correcto")
+    puntuacion += 10
+else:
+	print("Fallaste")
+	puntuacion = puntuacion - 5
+
+print("¿Quien entró primero en Berlin?")
+print("")
+print("a) La URSS")
+print("b) EEUU")
+print("c) Francia")
+print("")
+solucion = input("Introduzca su respuesta: ")
+
+if (solucion == "a"):
+    print("Correcto")
+    puntuacion += 10
+else:
+	print("Fallaste")
+	puntuacion = puntuacion - 5
+
+print("Nota final: "+ str(puntuacion))
