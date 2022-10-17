6e7a9133d48a6b38a3af3bd6c861b6209e36626c
OscarFrancoPerez <ofrancop@fpcoruna.afundacion.org>
2022-10-17 13:34:34 +0200

    Acabo de leer lo de principal.py

--- sprint2python/factorial.py
+++ sprint2python/factorial.py
@@ -1,11 +1,8 @@
 ﻿#factorial.py
 
-#Inicio de variables
-numero = input("Introduce un número para saber su factorial: ")
-factorial = 1
-
 #Funcion recursiva 
 def func_factorial(numero):
+	factorial = 1
 	#Salida del bucle
 	if numero == 1: return numero
 	#Input invalido
@@ -13,6 +10,3 @@ def func_factorial(numero):
 	#Continuación hasta volver entrar en el if
 	else: return numero*func_factorial(numero-1)
 
-#Resultado
-print("El factorial es : ")
-print(func_factorial(int(numero)))
