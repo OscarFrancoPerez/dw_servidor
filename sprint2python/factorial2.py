9388defdd2889d0ce398d9626398b9b68281e20d
OscarFrancoPerez <ofrancop@fpcoruna.afundacion.org>
2022-10-17 13:55:19 +0200

    principal.py corriendo los dos factoriales con tiempo

---
+++ sprint2python/factorial2.py
@@ -0,0 +1,10 @@
+﻿# factorial2.py
+
+# Función no recursiva
+def func_factorial2(numero):
+    factorial = 1
+
+    if int(numero) >= 1:
+        for i in range(1, int(numero)+1):
+            factorial = factorial * i
+    return factorial
--- sprint2python/principal.py
+++ sprint2python/principal.py
@@ -1,8 +1,32 @@
-﻿from factorial import func_factorial
+﻿import time
+from factorial import func_factorial
+from factorial2 import func_factorial2
 
 # Inicio de variables
 numero = input("Introduce un número para saber su factorial: ")
+print("Escoge una forma de calculo:")
+print()
+print("a) Recursión")
+print("b) Sin Recursión")
+option = input()
+print()
 
-# Resultado
-print("El factorial es : ")
-print(func_factorial(int(numero)))
+if option == 'a':
+	start_time = time.time()
+	# Resultado
+	print("El factorial es : ")
+	print(func_factorial(int(numero)))
+	end_time = time.time()
+	elapsed_time = end_time - start_time
+	print()
+	print('El tiempo de ejecución ha sido :' + str(elapsed_time) + ' s')
+if option == 'b':
+	start_time = time.time()
+	# Resultado
+	print("El factorial es : ")
+	print(func_factorial2(int(numero)))
+	end_time = time.time()
+	elapsed_time = end_time - start_time
+	print()
+	print('El tiempo de ejecución ha sido :' + str(elapsed_time) + ' s')
+
