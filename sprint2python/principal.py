0afd2f24ccfd67d845381f61dc92db8335bf9d19
Oscar Franco <oscar@ar-tronik.com>
2022-10-17 13:34:34 +0200

    Acabo de leer lo de principal.py

---
+++ sprint2python/principal.py
@@ -0,0 +1,8 @@
+﻿from factorial import func_factorial
+
+# Inicio de variables
+numero = input("Introduce un número para saber su factorial: ")
+
+# Resultado
+print("El factorial es : ")
+print(func_factorial(int(numero)))
