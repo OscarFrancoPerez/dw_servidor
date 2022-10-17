81c060dcb0b92f9137853e0e2345b0a80e1a40f8
OscarFrancoPerez <ofrancop@fpcoruna.afundacion.org>
2022-10-17 13:28:20 +0200

    factorial.py (sin trampas)

---
+++ sprint2python/factorial.py
@@ -0,0 +1,18 @@
+﻿#factorial.py
+
+#Inicio de variables
+numero = input("Introduce un número para saber su factorial: ")
+factorial = 1
+
+#Funcion recursiva 
+def func_factorial(numero):
+	#Salida del bucle
+	if numero == 1: return numero
+	#Input invalido
+	elif numero < 1: return ("NA")
+	#Continuación hasta volver entrar en el if
+	else: return numero*func_factorial(numero-1)
+
+#Resultado
+print("El factorial es : ")
+print(func_factorial(int(numero)))
