ac157da7341ffbdf0bcb363b3e42bf8f251c99db
OscarFrancoPerez <113417165+OscarFrancoPerez@users.noreply.github.com>
2022-10-24 12:47:14 +0200

    Update comment.php

--- sprint3sesiones/www/mysite/comment.php
+++ sprint3sesiones/www/mysite/comment.php
@@ -1,5 +1,13 @@
 <?php
 $db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
+
+session_start();
+$user_id_a_insertar = 'NULL';
+
+if (!empty($_SESSION['user_id'])) {
+  $user_id_a_insertar = $_SESSION['user_id'];
+}
+
 $juego_id = $_POST['juego_id'];
 $comentario = $_POST['comentario'];
 $query = "INSERT INTO tComentarios(comentario, juego_id, usuario_id, fecha)
