611a188e10fb39ce3c61599becb71c67c094cf15
Oscar Franco <ofrancop@fpcoruna.afundacion.org>
2022-10-25 12:59:23 +0200

    Updated files

--- sprint3sesiones/www/mysite/detail.php
+++ sprint3sesiones/www/mysite/detail.php
@@ -38,7 +38,7 @@
         $query2 = 'SELECT * FROM tComentarios WHERE juego_id='.$juego_id;
         $result2 = mysqli_query($db, $query2) or die('Query error');
         while ($row = mysqli_fetch_array($result2)) {
-          echo '<li>'.$row['fecha'].' : '.$row['comentario'].'</li>';
+          echo '<li>Posted by: '.$row['nombre'].' at '.$row['fecha'].' : '.$row['comentario'].'</li>';
         }
       ?>
       </ul>
