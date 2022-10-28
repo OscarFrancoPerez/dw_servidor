42e4d756a9a0f2a659d7873296469724a413386c
Oscar Franco <ofrancop@fpcoruna.afundacion.org>
2022-10-28 18:35:40 +0200

    Updated Files

--- sprint3sesiones/www/mysite/password_change.php
+++ sprint3sesiones/www/mysite/password_change.php
@@ -9,12 +9,12 @@
     $old_password_post = $_POST['old_password'];
     $new_password_post = $_POST['new_password'];
 
-    $query = 'SELECT id, contrase¤a FROM tUsuarios where id = "'.$user.'"';
+    $query = "SELECT contrase¤a FROM tUsuarios where id = '".$user."'";
     $result = mysqli_query($db, $query) or die('Query error');
     if (mysqli_num_rows($result)>0){
         $only_row = mysqli_fetch_array($result);
         if ($only_row[1] == $old_password_post){
-            $query2 = "UPDATE  tUsuarios SET contrase¤a = '".$new_password_post."' WHERE id =".$user.'; ';
+            $query2 = "UPDATE  tUsuarios SET contrase¤a = '".$new_password_post."' WHERE id ='".$user."'; ";
             $result = mysqli_query($db, $query2) or die('Query2 error');
             header('Location: main.php');
         } else{
