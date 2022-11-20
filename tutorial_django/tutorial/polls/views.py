8692fddd2c5074bfbb786db280d9163a71f90405
Óscar Franco <ofrancop@fpcoruna.afundacion.org>
2022-11-20 18:04:12 +0100

    Tutorial Django: Fix

--- tutorial_django/tutorial/polls/views.py
+++ tutorial_django/tutorial/polls/views.py
@@ -6,19 +6,19 @@ from django.urls import reverse
 
 # Create your views here.
 
-def IndexView(generic.ListView):
+class IndexView(generic.ListView):
 	template_name = 'polls/index.html'
 	context_object_name = 'latest_question_list'
 
 	def get_queryset(self):
-	"""Return the last five published questions."""
-	return Question.objects.order_by('-pub_date')[:5]
+		"""Return the last five published questions."""
+		return Question.objects.order_by('-pub_date')[:5]
 
-def DetailView(generic.DetailView):
+class DetailView(generic.DetailView):
 	model = Question
 	template_name = 'polls/detail.html'
 
-def ResultsView(generic.DetailView):
+class ResultsView(generic.DetailView):
 	model = Question
 	template_name = 'polls/results.html'
 
