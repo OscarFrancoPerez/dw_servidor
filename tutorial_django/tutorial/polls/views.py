3a8bd85d5309ec82802e57f81466ce78f3ff8615
Óscar Franco <ofrancop@fpcoruna.afundacion.org>
2022-11-20 17:01:52 +0100

    Tutorial Django 3: Fin Parte 3

--- tutorial_django/tutorial/polls/views.py
+++ tutorial_django/tutorial/polls/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from .models import Question
 from django.template import loader
+from django.http import Http404
 
 # Create your views here.
 
